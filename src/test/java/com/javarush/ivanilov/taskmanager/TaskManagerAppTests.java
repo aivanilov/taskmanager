7bdeca7ac3470e1fdcf04b02298eb8f964e32a1f
aivanilov <ivanilov@inbox.ru>
2023-06-16 12:13:34 +0700

    feat!: unversioned files has been added.

---
+++ src/test/java/com/javarush/ivanilov/taskmanager/TaskManagerAppTests.java
@@ -0,0 +1,13 @@
+package com.javarush.ivanilov.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
